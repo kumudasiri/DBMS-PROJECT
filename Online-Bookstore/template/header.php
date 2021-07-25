d139143400f7636070110ca66bfc9d167950bb4a
kumudasiri <70133242+kumudasiri@users.noreply.github.com>
2021-07-25 14:18:23 +0530

    Add files via upload

---
+++ Online-Bookstore/template/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?php echo $title; ?></title>
+
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="./bootstrap/css/jumbotron.css" rel="stylesheet">
+    <link href="./bootstrap/css/style.css"  rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Bookstore</a>
+        </div>
+
+        <!--/.navbar-collapse -->
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+              <!-- link to publiser_list.php -->
+              <li><a href="publisher_list.php"><span class="glyphicon glyphicon-pencil"></span>&nbsp; Publisher</a></li>
+              <!-- link to books.php -->
+              <li><a href="books.php"><span class="glyphicon glyphicon-book"></span>&nbsp; Books</a></li>
+              <!-- link to contacts.php -->
+              <li><a href="contact.php"><span class="glyphicon glyphicon-earphone"></span>&nbsp; Contact</a></li>
+              <!-- link to shopping cart -->
+              <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; My Cart</a></li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+    <?php
+      if(isset($title) && $title == "Index") {
+    ?>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+       <a href="index.php"></a>
+        <img src="bootstrap/img/book_logo.jpg" width="125" height="125"  style="float: left; margin: 10px 15px 10px 0">
+        <h1>Book Addicts</h1>
+        <p class="lead">A Book is a gift you can open again and again!!!</p>
+        <p>Books give a soul to the universe,wings to a mind and life to everything!!</p>
+      </div>
+    </div>
+    <?php } ?>
+
+    <div class="container" id="main">
